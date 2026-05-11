8d06759f23b3c97e7b135f1d99f7383e34c2a78a
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-05-11 12:07:59 +0600

    now, add onsubmit function for add data

--- src/app/add-destination/page.jsx
+++ src/app/add-destination/page.jsx
@@ -1,13 +1,25 @@
+'use client'
+
 import Container from "@/components/shared/Container";
 import { FieldError, Input, Label, TextField, Select, ListBox, TextArea, Button } from "@heroui/react";
 
 const AddDestinationPage = () => {
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const destination = Object.fromEntries(formData.entries());
+
+        console.log(destination);
+    };
+
     return (
         <div>
             <Container>
                 <form
+                    onSubmit={onSubmit}
                     className="py-20 space-y-8"
                 >
+                    <h1 className="text-2xl font-bold text-center">Add Destination</h1>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                         {/* Destination Name */}
                         <div className="md:col-span-2">
